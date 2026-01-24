921ed418b8c15f7d1ef16e9374149ebf73b28a4c
MiltonRafael <miltonrafael166@gmail.com>
2026-01-24 06:48:29 -0300

    seeding no h2 - desafio 2

--- desafio2/src/main/java/com/devsuperior/desafio2/entities/Participante.java
+++ desafio2/src/main/java/com/devsuperior/desafio2/entities/Participante.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,8 @@ public class Participante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique = true)
 	private String email;
 	
 	@ManyToMany(mappedBy = "participantes")
