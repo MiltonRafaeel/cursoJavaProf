74e2442ad4a27efb12ab682eed0ab786fc117260
MiltonRafael <miltonrafael166@gmail.com>
2026-01-28 20:46:05 -0300

    salvando entidades associadas para um - Part 2 (com nome aninhado)

--- modulo4/src/main/java/com/devsuperior/modulo4/services/PersonService.java
+++ modulo4/src/main/java/com/devsuperior/modulo4/services/PersonService.java
@@ -6,22 +6,28 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.modulo4.dto.PersonDepartmentDTO;
 import com.devsuperior.modulo4.entities.Department;
 import com.devsuperior.modulo4.entities.Person;
+import com.devsuperior.modulo4.repositories.DepartmentRepository;
 import com.devsuperior.modulo4.repositories.PersonRepository;
 
 @Service
 public class PersonService {
-	
+
 	@Autowired
 	private PersonRepository personRepository;
-	
-	public PersonDepartmentDTO insert (PersonDepartmentDTO dto) {
+
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
 		Person entity = new Person();
 		entity.setName(dto.getName());
 		entity.setSalary(dto.getSalary());
-		
-		Department dep = new Department();
-		dep.setId(dto.getDepartment().getId());
-		
+
+		Department dep = departmentRepository.findById(dto.getDepartment().getId()).get();
+
+		//Department dep = new Department();
+		//dep.setId(dto.getDepartment().getId());
+
 		entity.setDepartment(dep);
 		entity = personRepository.save(entity);
 		return new PersonDepartmentDTO(entity);
