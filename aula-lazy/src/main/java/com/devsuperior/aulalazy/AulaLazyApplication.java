fc83a2d785206e6f7b0fb6f7069e8be0a45a6581
MiltonRafael <miltonrafael166@gmail.com>
2026-01-29 16:15:18 -0300

    criando projeto para entender sobre a performance

---
+++ aula-lazy/src/main/java/com/devsuperior/aulalazy/AulaLazyApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulalazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaLazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaLazyApplication.class, args);
+	}
+
+}
