b24c5ddbf448fe401dd95efd811b559f2cc6338f
MiltonRafael <miltonrafael166@gmail.com>
2026-01-31 06:52:45 -0300

    aulao N mais UM - Busca paginada

---
+++ demo/src/main/java/com/devsuperior/demo/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.ProductDTO;
+import com.devsuperior.demo.entities.Product;
+import com.devsuperior.demo.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> find(PageRequest pageRequest) {
+		Page<Product> list = repository.findAll(pageRequest);
+		return list.map(x -> new ProductDTO(x));
+	}
+}
