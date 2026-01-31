1f16a52ed90c3982339feafb2804462c68977146
MiltonRafael <miltonrafael166@gmail.com>
2026-01-31 08:54:44 -0300

    consultas customizadas com performace sem paginacao

--- jpa-queries/src/main/java/com/example/jpa_queries/repositories/EmployeeRepository.java
+++ jpa-queries/src/main/java/com/example/jpa_queries/repositories/EmployeeRepository.java
@@ -1,10 +1,14 @@
 package com.example.jpa_queries.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.jpa_queries.entities.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-	
+	@Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department")
+	List<Employee> searchAll();
 }
--- jpa-queries/src/main/java/com/example/jpa_queries/repositories/ProductRepository.java
+++ jpa-queries/src/main/java/com/example/jpa_queries/repositories/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.example.jpa_queries.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.jpa_queries.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-	
+	@Query(value = "SELECT obj FROM Product obj JOIN FETCH obj.categories")
+	List<Product> searchAll();
 }
