9e08924e206a1a6fd4a5ced505afad4715cd1064
MiltonRafael <miltonrafael166@gmail.com>
2026-02-12 17:11:42 -0300

    salvando Order

--- dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscommerce.repositories;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
